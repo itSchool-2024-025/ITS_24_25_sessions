1ba4c851f905a5a1a059397a8a69270610b61b4e
DanIenasescu <dan.ienasescu@itschool.ro>
2024-10-02 19:51:43 +0300

    Updated main.py
    We include as of today generator functions, exceptions and importing funcitons from files (modules).

--- session_11/project_sesh_11/main.py
+++ session_11/project_sesh_11/main.py
@@ -7,7 +7,7 @@ from libs.lib_2 import *
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # print_hi('PyCharm')
-    list_1 = ["cat", "dog", "mouse"]
+    list_1 = ["cat"] #, "dog", "mouse"]
     # list_comprehension_funct([1, 2, 3, 4, 5, 6])
     # what_are_we_doing(list_1)
 
@@ -16,7 +16,24 @@ if __name__ == '__main__':
     from a given collection, and, if en error arises, prompt the user and exit the loop, without
     any "red text".
     """
-    var = input("Message to user")
     my_single_obj = generator_function(list_1)
-    print(type(my_single_obj))
+    while True:
+        var = input("Message to user")
+        my_set = {"dog"}
+        # if
+        if var == "Yes":
+            try:
+                print(next(my_single_obj))
+                my_set[0] = "mouse"
+            except (StopIteration, ZeroDivisionError, TypeError) as e:
+                print("Rwo-Oh!")
+                print(f"An error occurred: {e}")
+            else:
+                print("Now this has to work!")
+            finally:
+                print("OOoooookay!")
+        else:
+            print("Bye!")
+            break
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
